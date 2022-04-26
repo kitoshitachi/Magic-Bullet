6548f0fe31a888e77428c0511e3904ba616831f0
kitoshitachi <19521204@gm.uit.edu.vn>
2022-04-26 15:56:18 +0700

    clean code

--- code/player.py
+++ code/player.py
@@ -1,28 +1,26 @@
 import pygame
 from settings import *
 from math import atan2,degrees
-from bullet import Bullet
+from debug import debug
 class Player(pygame.sprite.Sprite):
-	def __init__(self, pos, groups, visible_sprites, obstacle_sprites,create_bullet):
+	def __init__(self, pos, groups,visible_sprites:pygame.sprite.Group,obstacle_sprites:pygame.sprite.Group,create_shoot):
 		super().__init__(groups)
 		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft=pos)
 		self.hitbox = self.rect.inflate(0,-26)
-
 		self.visible_sprites = visible_sprites
 		self.obstacle_sprites = obstacle_sprites
-
 		#movement
-		self.direction = pygame.math.Vector2()
+		self.direction = pygame.math.Vector2(0,0)
 		self.speed = 5
+		self.angle = 0
 		self.attacking = False
 		self.attack_cooldown = 400
 		self.attack_time = None
-		self.angle = 0
-		self.create_bullet = create_bullet
+		self.shoot = create_shoot
+
 		#rotate
 		self.original_image = self.image
-
 
 	def input(self):
 		keys_press = pygame.key.get_pressed()
@@ -39,11 +37,13 @@ class Player(pygame.sprite.Sprite):
 			self.direction.x = -1
 		else:
 			self.direction.x = 0
+		
 
-		if keys_press[pygame.K_SPACE] and not self.attacking:
+		left_mouse_pressed = pygame.mouse.get_pressed()[0]
+		if left_mouse_pressed and not self.attacking:
 			self.attacking = True 
 			self.attack_time = pygame.time.get_ticks()
-			self.create_bullet()
+			self.shoot()
 		
 		
 	def rotate_player(self):
@@ -54,36 +54,34 @@ class Player(pygame.sprite.Sprite):
 		self.image = pygame.transform.rotate(self.original_image, -self.angle)
 		self.rect = self.image.get_rect(center=original_rect.center)
 
-	def move(self, speed):
+	def move(self,speed):
 		if self.direction.magnitude() != 0:
-			self.direction = self.direction.normalize()
+				self.direction = self.direction.normalize()
 
 		self.hitbox.x += self.direction.x * speed
-		self.collision('horizontal')
+		self.collision_horizontal()
+				
 		self.hitbox.y += self.direction.y * speed
-		self.collision('vertical')
+		self.collision_vertical()
+
 		self.rect.center = self.hitbox.center
 
-	def shoot(self):
-		bullet = Bullet(self.rect,self.angle)
+	def collision_horizontal(self):
+		for sprite in self.obstacle_sprites:
+			if sprite.hitbox.colliderect(self.hitbox):
+				if self.direction.x > 0:  # moving right
+					self.hitbox.right = sprite.hitbox.left
+					
+				elif self.direction.x < 0:  # moving left
+					self.hitbox.left = sprite.hitbox.right
 		
-
-	def collision(self, direction):
-		if direction == 'horizontal':
-			for sprite in self.obstacle_sprites:
-				if sprite.hitbox.colliderect(self.hitbox):
-					if self.direction.x > 0:  # moving right
-						self.hitbox.right = sprite.hitbox.left
-					if self.direction.x < 0:  # moving left
-						self.hitbox.left = sprite.hitbox.right
-
-		if direction == 'vertical':
-			for sprite in self.obstacle_sprites:
-				if sprite.hitbox.colliderect(self.hitbox):
-					if self.direction.y > 0: # moving down
-						self.hitbox.bottom = sprite.hitbox.top
-					if self.direction.y < 0: # moving up
-						self.hitbox.top = sprite.hitbox.bottom
+	def collision_vertical(self):
+		for sprite in self.obstacle_sprites:
+			if sprite.hitbox.colliderect(self.hitbox):
+				if self.direction.y > 0:  # moving down
+					self.hitbox.bottom = sprite.hitbox.top
+				elif self.direction.y < 0:  # moving up
+					self.hitbox.top = sprite.hitbox.bottom
 
 	def cooldown(self):
 		current_time = pygame.time.get_ticks()
@@ -95,6 +93,5 @@ class Player(pygame.sprite.Sprite):
 	def update(self):
 		self.input()
 		self.cooldown()
-		
 		self.move(self.speed)
 		self.rotate_player()
