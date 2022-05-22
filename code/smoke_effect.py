f80a4cdba9c20cfa34d7c55210d0bf752e9abbf4
kitoshitachi <19521204@gm.uit.edu.vn>
2022-05-22 17:29:54 +0700

    docstring smoke effect

--- code/smoke_effect.py
+++ code/smoke_effect.py
@@ -7,12 +7,20 @@ class SmokeEffect(GameObject):
 	FIRE_SFX.set_volume(0.4)
 
 	def __init__(self, pos, level):
+		'''
+		make effect of bullet
+		param pos: give pos of bullet
+		param level: take group sprite
+		'''
 		super().__init__(level, [level.group_visible], Assets.smoke_effect[0], pos=pos)
 		self.animation = SpriteAnimation(self, Assets.smoke_effect, loop=False)
 
 		SmokeEffect.FIRE_SFX.play();
 
 	def update(self, delta_time):
+		'''
+		update the animation
+		'''
 		super().update(delta_time)
 		self.animation.update(delta_time)
 
