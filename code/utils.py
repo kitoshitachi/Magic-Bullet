2998a62df57a5b8c2344a4c9c961f33ebd376e5a
kitoshitachi <19521204@gm.uit.edu.vn>
2022-05-22 18:28:37 +0700

    docstring utils

--- code/utils.py
+++ code/utils.py
@@ -1,13 +1,16 @@
 from math import sqrt,floor,ceil
 
 class Utils:
+	'''class utils'''
 	@staticmethod
 	def distance(pos_1, pos_2):
+		'''caculator distance of two points'''
 		dx = pos_1[0] - pos_2[0]
 		dy = pos_1[1] - pos_2[1]
 		return sqrt(dx**2 + dy**2)
 
 	@staticmethod
 	def round_away_from_zero(x):
+		'''round x, mixing value if value = 0.5'''
 		return int(floor(x + 0.5) if x > 0 else ceil(x - 0.5))
 
