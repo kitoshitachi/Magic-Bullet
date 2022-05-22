712f6f3ab7b0c659e507fa3a14da244050c23dd5
kitoshitachi <19521204@gm.uit.edu.vn>
2022-05-22 17:56:30 +0700

    docstring obstacle

--- code/Obstacle.py
+++ code/Obstacle.py
@@ -1,18 +1,24 @@
-from math import floor
 import pygame
-from assets import Assets
 from game_object import GameObject
 from settings import *
 
-
 class Obstacle(GameObject):
-  def __init__(self, image, area, pos, hitbox, level):
-    super().__init__(level=level, group=[level.group_visible, level.group_obstacle])
+	def __init__(self, image, area, pos, hitbox, level):
+		'''make obstacle sprite
+		param image: image of obstacle
+		param area: area of obstacle, to draw obstacle
+		param pos: pos of obstacle
+		param level: get attribute of level playing'''
+		super().__init__(level=level, group=[level.group_visible, level.group_obstacle])
 
-    self.image = image
-    self.rect = pygame.Rect(pos[0], pos[1], area[2], area[3])
-    self.hitbox = hitbox
-    self.area = area
+		self.image = image
+		self.rect = pygame.Rect(pos[0], pos[1], area[2], area[3])
+		self.hitbox = hitbox
+		self.area = area
 
-  def render(self, camera):
-    camera.surface.blit(self.image, camera.apply(self), self.area)
+	def render(self, camera):
+		'''
+		render image in camera
+		param camera: take surface camera to render
+		'''
+		camera.surface.blit(self.image, camera.apply(self), self.area)
