2dcbe386841566485368d9970554f7a48ea078e0
kitoshitachi <19521204@gm.uit.edu.vn>
2022-05-22 17:58:51 +0700

    docstring boundary

--- code/boundary.py
+++ code/boundary.py
@@ -1,16 +1,18 @@
-from math import floor
-import pygame
-from assets import Assets
 from game_object import GameObject
 from settings import *
 
 
 class Boundary(GameObject):
-  def __init__(self, pos, hitbox, level):
-    super().__init__(level, group=[level.group_boundary])
+	def __init__(self, pos, hitbox, level):
+		'''
+		make boundary of map
+		param pos: pos of boundaray
+		param level: get attribute level
+		'''
+		super().__init__(level, group=[level.group_boundary])
 
-    self.rect = hitbox.copy()
-    self.hitbox = hitbox
+		self.rect = hitbox.copy()
+		self.hitbox = hitbox
 
-  def render(self, offset):
-    pass
+	def render(self, offset):
+		pass
